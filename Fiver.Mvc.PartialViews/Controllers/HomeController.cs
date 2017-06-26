8e707e6d49ad54cc76a48518238e6708b7f0acbb
Tahir Naushasd <t.naushad25@gmail.com>
2017-06-26 09:57:57 +0100

    setup

---
+++ Fiver.Mvc.PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fiver.Mvc.PartialViews.Models.Home;
+
+namespace Fiver.Mvc.PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new EmployeeViewModel
+            {
+                Id = 1,
+                Firstname = "James",
+                Surname = "Bond",
+                Address = new AddressViewModel
+                {
+                    Line1 = "Secret Location",
+                    Line2 = "London",
+                    Line3 = "UK"
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Fiver.Mvc.PartialViews/Models/Home/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Fiver.Mvc.PartialViews.Models.Home
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public AddressViewModel Address { get; set; }
+    }
+
+    public class AddressViewModel
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+    }
+}
